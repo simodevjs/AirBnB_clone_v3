fcb0533c9b14b8b46a3263bb790e941e4a604a78
simodevjs <simodevjs@gmail.com>
2024-05-23 00:52:49 +0100

    ddzdzd

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,21 @@
 from flask import Flask, render_template, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flasgger import Swagger
+
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+Swagger(app)
+
+
 
 @app.teardown_appcontext
 def close_storage(exception):
@@ -28,3 +40,13 @@ if __name__ == "__main__":
             port=os.getenv('HBNB_API_PORT', 5000),
             threaded=True)
 
+"""
+    if __name__ == "__main__":
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
+"""
