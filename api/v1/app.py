3a43a381b33ba399e3dba2d4f6d756eee52e3264
simodevjs <simodevjs@gmail.com>
2024-05-22 18:30:09 +0100

    dzdzdzd

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+#!/usr/bin/python3
+""" Flask Application """
+from flask import Flask, render_template, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -9,6 +11,16 @@ app.register_blueprint(app_views)
 def close_storage(exception):
     """Close database session."""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
     import os
--- apizzz/v1/app.py
+++ apizzz/v1/app.py
@@ -2,8 +2,8 @@
 """ Flask Application """
 from models import storage
 from api.v1.views import app_views
-from os import environ
 from flask import Flask, render_template, make_response, jsonify
+from os import environ
 from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
